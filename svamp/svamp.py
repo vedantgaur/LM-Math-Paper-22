054b9433b3b9de45aa0c0229d9a10635516eb1ca
Vedant Gaur <vedantgaur101@gmail.com>
2022-07-08 13:57:37 -0700

    fix issues with dataset symbolizing

--- svamp/svamp.py
+++ svamp/svamp.py
@@ -22,22 +22,27 @@ def parse(index):
     variables = []
 
     for i in range(len(nums)):
-        num_index = problem.find(nums[i])
-        equation_index = equation.find(nums[i])
+        num_index = problem.find(f"{nums[i]} ")
+        equation_index = equation.find(f"{nums[i]}.0")
         variables.append(f"<{i+1}>")
 
         while num_index != -1:
             for j in range(len(nums[i])):
                 problem = problem[:num_index] + problem[num_index+1:]
             problem = problem[:num_index] + variables[i] + problem[num_index:]
-            num_index = problem.find(nums[i])
+            num_index = problem.find(f"{nums[i]} ")
+            print(problem)
+            # print(f"Done with problem {i}")
 
-        while equation_index != -1:
-            # if equation_index != -1:
-            for k in range(len(nums[i])+2):
-                equation = equation[:equation_index] + equation[equation_index+1:]
-            equation = equation[:equation_index] + variables[i] + equation[equation_index:]
-            equation_index = equation.find(nums[i])
+        if equation_index != -1:
+            while equation_index != -1:
+                for k in range(len(nums[i])+2):
+                    equation = equation[:equation_index] + equation[equation_index+1:]
+                equation = equation[:equation_index] + variables[i] + equation[equation_index:]
+                print(f"{nums[i]}.0")
+                equation_index = equation.find(f"{nums[i]}.0")
+                print(equation + "\n")
+                # print(f"Done with equation {i}")
 
     # print(problem)
     # print(equation)
@@ -46,12 +51,18 @@ def parse(index):
 
 
 if __name__ == "__main__":
+    print(len(df))
     data = pd.DataFrame(columns=["Symbolic Problem", "Symbolic Equation", "Variables", "Numbers"])
-    for i in range(len(df)):
-    # for i in range(2):
-        symbolic_problem, symbolic_equation, variables, numbers = parse(i)
-        datapoints = pd.DataFrame({"Symbolic Problem": [symbolic_problem], "Symbolic Equation": [symbolic_equation], "Variables": [variables], "Numbers": [numbers]})
+    # for _ in range(len(df)):
+    for _ in range(28):
+        symbolic_problem, symbolic_equation, variables, numbers = parse(_)
+        datapoints = pd.DataFrame({
+                                   "Symbolic Problem": [symbolic_problem],
+                                   "Symbolic Equation": [symbolic_equation], 
+                                   "Variables": [variables], 
+                                   "Numbers": [numbers]
+                                 })
         data = pd.concat([data, datapoints], ignore_index = True)
         # print(data)
-        print(f"Done with Question {i+1}\n")
+        print(f"Done with Question {_+1}\n")
     data.to_csv("svamp/svamp_results/temp_dataset.csv")
