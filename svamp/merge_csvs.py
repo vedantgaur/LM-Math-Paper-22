9317b5645f3b5c5dd013c1745470a2e048557bf2
Vedant Gaur <vedantgaur101@gmail.com>
2022-07-09 18:28:51 -0700

    finish dataset symbolization

--- svamp/merge_csvs.py
+++ svamp/merge_csvs.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# df = pd.read_csv("SVAMP.csv")
-# df1 = pd.read_csv("temp.csv")
+df = pd.read_csv("svamp/SVAMP.csv")
+df1 = pd.read_csv("svamp/svamp_results/temp_dataset.csv")
 
-# data = df.join(df1)
-# print(data)
-# data.to_csv("augmented_svamp.csv")
-df = pd.read_csv("augmented_svamp.csv")
-df.drop(df.columns[1], axis=1, inplace=True)
-df.to_csv("augmented_svamp.csv")
+data = df.join(df1)
+print(data)
+data.to_csv("svamp/svamp_results/augmented_svamp.csv")
+# df = pd.read_csv("augmented_svamp.csv")
+# df.drop(df.columns[1], axis=1, inplace=True)
+# df.to_csv("augmented_svamp.csv")
--- svamp/symbolize_dataset.py
+++ svamp/symbolize_dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ast
 import numpy as np
 
-vars = [
+replacements = [
     "w", "x", "y", "z"
 ]
 
@@ -10,26 +10,46 @@ df = pd.read_csv("svamp/svamp_results/augmented_svamp.csv")
 
 symbolic_problems = df["Symbolic Problem"]
 variables = df["Variables"]
+symbolic_equations = df["Symbolic Equation"]
 
 def symbolize_dataset(index):
-    var_index = 0
     problem = symbolic_problems[index]
+    equation = symbolic_equations[index]
     vars = variables[index]
-    vars = ast.literal_eval(vars) 
+    vars = ast.literal_eval(vars)
+    print(problem)
 
     for i in range(len(vars)):
-        while var_index == -1:
+        var_index = problem.find(vars[i])
+        while var_index != -1:
+            if var_index != -1:
+                for j in range(len(vars[i])):
+                    problem = problem[:var_index] + problem[var_index+1:]
+                problem = problem[:var_index] + replacements[i] + problem[var_index:]
+                print(problem)
+                print(var_index)
             var_index = problem.find(vars[i])
-            for j in range(len(vars[i])):
-                problem = problem[:var_index] + problem[var_index+1:]
-            problem = problem[:var_index] + f"x{i+1}" + problem[var_index:]
-    return problem
+        
+        equation_index = equation.find(vars[i])
+        if equation_index != -1:
+            while equation_index != -1:
+                for k in range(len(vars[i])):
+                    equation = equation[:equation_index] + equation[equation_index+1:]
+                equation = equation[:equation_index] + replacements[i] + equation[equation_index:]
+                equation_index = equation.find(vars[i])
+                if equation_index != -1:
+                    equation_index += 1
+                print(equation + "\n")
+
+    return problem, equation
 
 
 if __name__ == "__main__":
-    data = pd.DataFrame(columns=["Question"], index=np.arange(len(variables)))
+    data = pd.DataFrame(columns=["Question", "Equation"], index=np.arange(len(variables)))
     for i in range(len(variables)):
-        data["Question"][i] = symbolize_dataset(i)
+    # for i in range(11):
+        data["Question"][i], data["Equation"][i] = symbolize_dataset(i)
+        print(data["Question"][i])
         # questions = pd.DataFrame([symbolize_dataset(i)], columns=["Question"])
         # pd.concat([data, questions], ignore_index=True)
-    data.to_csv("svamp/svamp_results/test.csv")
+    data.to_csv("svamp/svamp_results/wxyz_dataset.csv")
